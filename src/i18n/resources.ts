96ea0b0aa9ad0250b622bf45c102becbe126d8d5
qqharry21 <qqharry21@gmail.com>
2025-05-16 23:56:36 +0800

    feat: add English and Traditional Chinese alternatives localization files for food, cool items, and travel

--- src/i18n/resources.ts
+++ src/i18n/resources.ts
@@ -1,6 +1,7 @@
+import enAlternatives from '../../locales/en/alternatives.json';
 import en from '../../locales/en/message.json';
 import enZod from '../../locales/en/zod.json';
-
+import zh_TWAlternatives from '../../locales/zh_TW/alternatives.json';
 import zh_TW from '../../locales/zh_TW/message.json';
 import zh_TWZod from '../../locales/zh_TW/zod.json';
 
@@ -8,10 +9,12 @@ const resources = {
   en: {
     translation: en,
     zod: enZod,
+    alternatives: enAlternatives,
   },
   zh_TW: {
     translation: zh_TW,
     zod: zh_TWZod,
+    alternatives: zh_TWAlternatives,
   },
 };
 
